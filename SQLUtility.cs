7d462cf3a7f80af184a6c6363638c47710e47e9e
chanamindy <chanamindyh@gmail.com>
2025-03-25 18:13:24 -0400

    SQL command procedure

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
 
 namespace CPUFramework
 {
@@ -8,19 +9,74 @@ namespace CPUFramework
     {
         public static string ConnectionString = "";
 
+        public static SqlCommand GetSqlCommand(string sprocname)
+        {
+            SqlCommand cmd;
+            using (SqlConnection conn = new SqlConnection(SQLUtility.ConnectionString))
+            {
+                cmd = new SqlCommand(sprocname, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                conn.Open();
+                SqlCommandBuilder.DeriveParameters(cmd);
+            }
+            return cmd;
+        }
+
+        public static DataTable GetDataTable(SqlCommand cmd)
+        {
+            Debug.Print("------------" + Environment.NewLine + cmd.CommandText);
+            DataTable dt = new();
+            using (SqlConnection conn = new SqlConnection(SQLUtility.ConnectionString))
+            {
+                conn.Open();
+                cmd.Connection = conn;
+                SqlDataReader dr = cmd.ExecuteReader();
+                dt.Load(dr);
+            }
+            //SetAllColumnsAllowNull(dt);
+            return dt;
+        }
+
         public static DataTable GetDataTable(string sqlstatement)
         {
-            Debug.Print(sqlstatement);
-            DataTable dt = new();
-            SqlConnection conn = new();
-            conn.ConnectionString = ConnectionString;
-            conn.Open();
-            var cmd = new SqlCommand();
-            cmd.Connection = conn;
-            cmd.CommandText = sqlstatement;
-            var dr = cmd.ExecuteReader();
-            dt.Load(dr);
-            return dt;
+            return GetDataTable(new SqlCommand(sqlstatement));
+        }
+
+        public static void ExecuteSQL(string sqlstatement)
+        {
+            GetDataTable(sqlstatement);
+        }
+
+        public static int GetFirstColumnFirstRowValue(string sql)
+        {
+            int n = 0;
+
+            DataTable dt = GetDataTable(sql);
+            if (dt.Rows.Count > 0 && dt.Columns.Count > 0)
+            {
+                string s = dt.Rows[0][0].ToString();
+                int.TryParse(s, out n);
+            }
+            return n;
+        }
+
+        private static void SetAllcolumnsAllowNull(DataTable dt)
+        {
+            foreach (DataColumn c in dt.Columns)
+            {
+                c.AllowDBNull = true;
+            }
+        }
+
+        public static void DebugPrintDataTable(DataTable dt)
+        {
+            foreach (DataRow r in dt.Rows)
+            {
+                foreach (DataColumn c in dt.Columns)
+                {
+                    Debug.Print(c.ColumnName + " = " + r[c.ColumnName].ToString());
+                }
+            }
         }
     }
 }
