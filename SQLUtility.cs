1bc66ffe7c58cd3eb5b0e32495012d4d6beec6af
chanamindy <chanamindyh@gmail.com>
2025-04-23 23:22:38 -0400

    HW #20 cpuframework

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -1,7 +1,7 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
-using System.Reflection.Metadata.Ecma335;
+using System.Text;
 
 namespace CPUFramework
 {
@@ -24,14 +24,22 @@ namespace CPUFramework
 
         public static DataTable GetDataTable(SqlCommand cmd)
         {
-            Debug.Print("------------" + Environment.NewLine + cmd.CommandText);
             DataTable dt = new();
             using (SqlConnection conn = new SqlConnection(SQLUtility.ConnectionString))
             {
                 conn.Open();
                 cmd.Connection = conn;
-                SqlDataReader dr = cmd.ExecuteReader();
-                dt.Load(dr);
+                Debug.Print(GetSQL(cmd));
+                try
+                {
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    dt.Load(dr);
+                }
+                catch (SqlException ex)
+                {
+                    string msg = ParseConstraintMsg(ex.Message);
+                    throw new Exception(msg);
+                }
             }
             SetAllcolumnsAllowNull(dt);
             return dt;
@@ -47,6 +55,53 @@ namespace CPUFramework
             GetDataTable(sqlstatement);
         }
 
+        private static string ParseConstraintMsg(string msg)
+        {
+            string origmsg = msg;
+            string prefix = "ck_";
+            string msgend = "";
+            if (msg.Contains(prefix) == false)
+            {
+                if (msg.Contains("u_"))
+                {
+                    prefix = "u_";
+                    msgend = " must be unique.";
+                }
+                else if (msg.Contains("f_"))
+                {
+                    prefix = "f_";
+                }
+            }
+            if (msg.Contains(prefix))
+            {
+                msg = msg.Replace("\"", "\'");
+                int pos = msg.IndexOf(prefix) + prefix.Length;
+                msg = msg.Substring(pos);
+                pos = msg.IndexOf("\'");
+
+                if (pos == -1)
+                {
+                    msg = origmsg;
+                }
+                else if (prefix == "f_")
+                {
+                    int poscurrenttableend = msg.IndexOf("_");
+                    string currenttable = msg.Substring(0, poscurrenttableend);
+                    int posforeigntableend = msg.IndexOf("\'");
+                    string foreigntable = msg.Substring(0, posforeigntableend);
+                    foreigntable = foreigntable.Substring(poscurrenttableend + 1);
+                    msg = "There is a reference in table " + foreigntable + " to this table " + currenttable + ", therefore this record can't be deleted.";
+                }
+                else
+                {
+                    msg = msg.Substring(0, pos);
+                    msg = msg.Replace("_", " ");
+                    msg = msg + msgend;
+                }
+            }
+            return msg;
+        }
+
         public static int GetFirstColumnFirstRowValue(string sql)
         {
             int n = 0;
@@ -54,8 +109,10 @@ namespace CPUFramework
             DataTable dt = GetDataTable(sql);
             if (dt.Rows.Count > 0 && dt.Columns.Count > 0)
             {
-                string s = dt.Rows[0][0].ToString();
-                int.TryParse(s, out n);
+                if (dt.Rows[0][0] != DBNull.Value)
+                {
+                    int.TryParse(dt.Rows[0][0].ToString(), out n);
+                }
             }
             return n;
         }
@@ -66,6 +123,46 @@ namespace CPUFramework
             {
                 c.AllowDBNull = true;
             }
+        }
+
+        public static string GetSQL(SqlCommand cmd)
+        {
+            string val = "";
+#if DEBUG
+            StringBuilder sb = new StringBuilder();
+            if (cmd.Connection != null)
+            {
+                sb.AppendLine($"--{cmd.Connection.DataSource}");
+                sb.AppendLine($"use {cmd.Connection.Database}");
+                sb.AppendLine("go");
+            }
+
+            if (cmd.CommandType == CommandType.StoredProcedure)
+            {
+                sb.AppendLine($"exec {cmd.CommandText}");
+                int paramcount = cmd.Parameters.Count - 1;
+                int paramnum = 0;
+                string comma = ",";
+                foreach (SqlParameter p in cmd.Parameters)
+                {
+                    if (p.Direction != ParameterDirection.ReturnValue)
+                    {
+                        if (paramnum == paramcount)
+                        {
+                            comma = "";
+                        }
+                        sb.AppendLine($"{p.ParameterName} = {(p.Value == null ? "null" : p.Value.ToString())}{comma}");
+                    }
+                    paramnum++;
+                }
+            }
+            else
+            {
+                sb.AppendLine(cmd.CommandText);
+            }
+            val = sb.ToString();
+#endif
+            return val;
         }
 
         public static void DebugPrintDataTable(DataTable dt)
